b0d6926c9daacd319b304b78adcd944353b154b3
satsbeha <79467896+satsbeha@users.noreply.github.com>
2021-03-28 18:22:54 -0700

    Destructuring assignmen and date

---
+++ cs303/labW1D6/APPFILE.js
@@ -0,0 +1,149 @@
+"use strict";
+module.exports = { topSalary };
+//Destructuring assignment
+
+/**
+ *assign viarable to the property of the object.
+ * @param  {object} user object
+ * @returns {string} returns varibales
+ * 
+ */
+//We have an object:
+
+let user = {
+  name: "John",
+  years: 30
+};
+/*
+Write the destructuring assignment that reads:
+
+name property into the variable name.
+years property into the variable age.
+isAdmin property into the variable isAdmin
+ (false, if no such property)
+ */
+
+
+  
+  let {name, years: age, isAdmin = false} = user;
+  
+  console.log( name ); // John
+  console.log( age ); // 30
+  console.log( isAdmin ); // false
+
+  // 2 The maximal salary
+
+
+  /**
+ *finds the name of the top-paid person.
+ * @param  {number} salaries salaries of individual
+ * @returns {string} returns the name of the top-paid person.
+ * 
+ */
+
+function topSalary(salaries) {
+
+    let max = 0;
+    let maxName = null;
+  
+    for(const [name, salary] of Object.entries(salaries)) {
+      if (max < salary) {
+        max = salary;
+        maxName = name;
+      }
+    }
+  
+    return maxName;
+  }
+
+  let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+  };
+
+  console.log(topSalary(salaries));
+  
+
+  //Section: Dates
+
+  //1 Create a date
+
+  //Create new date
+/* The new Date constructor uses the local time zone. So the only important thing to 
+ * remember is that months start from zero.So February has number 1.
+ */
+ 
+let mydate = new Date(2012, 1, 20, 3, 12);
+console.log( mydate );
+
+
+
+/** The method date.getDay() returns the number of the weekday, starting from sunday.
+ *  Let’s make an array of weekdays, so that we can get the proper day name by its number
+ * @param  {date} date input date
+ * @return {string} return proper day name
+ */
+function getWeekDay(date) {
+    let days = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
+  
+    return days[date.getDay()];
+  }
+  
+  let date = new Date(2014, 0, 3); // 3 Jan 2014
+  console.log( getWeekDay(date) ); // FR
+
+
+  /**European countries have days of week starting with Monday (number 1), then Tuesday (number 2) 
+   * and till Sunday (number 7). Write a function getLocalDay(date) that returns the “European” day 
+   * of week for date.
+   * @param  {date} date european date
+   * @returns {string} returns the “European” day of week for date.
+   */
+  function getLocalDay(date) {
+
+    let day = date.getDay();
+  
+    if (day == 0) { // weekday 0 (sunday) is 7 in european
+      day = 7;
+    }
+  
+    return day;
+  }
+    date = new Date(2012, 0, 3);  // 3 Jan 2012
+    console.log( getLocalDay(date) );       // tuesday, should show 2
+
+
+  /**Which day of month was many days ago?
+   * 
+   * @param  {date} date  date
+   * @param  {number} days given number of days
+   * @returns {number} return the day of month days ago from the date
+   */
+  function getDateAgo(date, days) {
+    let dateCopy = new Date(date);
+  
+    dateCopy.setDate(date.getDate() - days);
+    return dateCopy.getDate();
+  }
+
+console.log( getDateAgo(date, 1) ); 
+console.log( getDateAgo(date, 2) ); 
+console.log( getDateAgo(date, 365) ); 
+
+
+/**How many seconds has passed today?
+ * @returns {number} eturns the number of seconds from the beginning of today.
+ * 
+ */
+function getSecondsToday() {
+    let now = new Date();
+  
+    // create an object using the current day/month/year
+    let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  
+    let diff = now - today; // ms difference
+    return Math.round(diff / 1000); // make seconds
+  }
+  
+  console.log( getSecondsToday() );
--- cs303/labW1D6/TESTFILE.js
+++ cs303/labW1D6/TESTFILE.js
@@ -1,4 +1,10 @@
 "use strict";
+//const assert = require("assert");
+//const myExports = require("./APPFILE.js");
+//const topSalary = myExports.topSalary;
+//const getWeekDay = myExports.getWeekDay;
+//const getLocalDay = myExports.getLocalDay;
+//const getDateAgo = myExports.getDateAgo;
 /**test for finds the name of the top-paid person
  * @param  {string} "" a description about the test
  * @param  {function} function test a unit case
@@ -22,8 +28,40 @@ describe("Top Salary", function () {
     });
 
 
-    it("returns null for the empty object ", function () { assert.isNull(topSalary({})); });
+    it("returns null for the empty object ", function () { assert.strictEqual( topSalary({}), null); });
 
 });
+describe("Get a WeekDay", function () {
+
+
+
+    it("3 January 2014 - friday ", function () { assert.equal(getWeekDay(new Date(2014, 0, 3)), "FR"); });
+    it("4 January 2014 - saturday ", function () { assert.equal(getWeekDay(new Date(2014, 0, 4)), "SA"); });
+    it("5 January 2014 - sunday", function () { assert.equal(getWeekDay(new Date(2014, 0, 5)), "SU"); });
+
+});
+
+describe("Get European day of week for date", function () {
+
+
+
+    it("3 January 2014 - friday ", function () { assert.equal(getLocalDay(new Date(2014, 0, 3)), 5); });
+    it("4 January 2014 - saturday ", function () { assert.equal(getLocalDay(new Date(2014, 0, 4)), 6); });
+    it("5 January 2014 - sunday", function () { assert.equal(getLocalDay(new Date(2014, 0, 5)), 7); });
+
+});
+
+
+
+describe("Check Which day of month was many days ago?", function () {
+
+
+
+    it("1 day before 02.01.2015 -> day 1  ", function () { assert.equal(getDateAgo(new Date(2015, 0, 2), 1), 1); });
+    it("2 days before 02.01.2015 -> day 31  ", function () { assert.equal(getDateAgo(new Date(2015, 0, 2), 2), 31); });
+    it("100 days before 02.01.2015 -> day 24 ", function () { assert.equal(getDateAgo(new Date(2015, 0, 2), 100), 24); });
+
+});
+
 
 
