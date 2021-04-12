4a8810f4a3e9b523e7b43d2f309ab256f72da5e5
satsbeha <79467896+satsbeha@users.noreply.github.com>
2021-04-12 13:44:31 -0700

    amended

--- cs303/labW3D5/TESTFILE.js
+++ cs303/labW3D5/TESTFILE.js
@@ -4,10 +4,11 @@
 /* You need the assert and function name declarations to test in node.  
 Comment these out when you send it to the browser with the index.html mocha setup page.    */
 
-//const assert = require("assert");  //always need this with node
-//const myExports = require("./APPFILE.js");  //with node need the name of your file with your functions here
-//const Person = myExports.Person;  //do this for all of the functions used in the Mocha tests
-//const Teacher = myExports.Teacher; 
+const assert = require("assert");  //always need this with node
+const myExports = require("./APPFILE.js");  //with node need the name of your file with your functions here
+const quiz = myExports.quiz;  //do this for all of the functions used in the Mocha tests
+const Teacher = myExports.Teacher; 
+//const Person = myExports.quiz.scoreStudent;
 
 
 describe("test inheritance from Person", function () {
@@ -23,5 +24,18 @@ describe("test inheritance from Person", function () {
         bob.initialize("Bob", 25);
         assert.strictEqual(bob.teach("Physics"), "Bob is now teaching Physics");
     });
-
-});
+    describe("grades from quiz", function () {
+        it("scoreStudent 10", function () {
+            assert.strictEqual(quiz.scoreStudent(10), 1);
+        });
+        it("scoreStudent 11", function () {
+            assert.strictEqual(quiz.scoreStudent(11), 2);
+        });
+        it("scoreStudent 12", function () {
+            assert.strictEqual(quiz.scoreStudent(12), 2);
+        });
+        it("getAverage", function () {
+            assert.strictEqual(+quiz.getAverage().toFixed(3), 1.667);
+        });
+    });
+})
